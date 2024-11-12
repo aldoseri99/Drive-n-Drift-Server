227e070fec51596f53a204e005bbefd8d62bbd39
AlheloM <malhelo81@gmail.com>
2024-11-12 12:47:32 +0300

    yes

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,4 +1,6 @@
 const { Booking, Insurance } = require("../models")
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 
 const GetBookings = async (req, res) => {
   try {
@@ -49,9 +51,48 @@ const DeleteBooking = async (req, res) => {
   }
 }
 
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS
+  }
+})
+const sendAutoReplyEmail = async (emails, subject, text) => {
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: emails,
+    subject: subject,
+    text : text
+  }
+  console.log(process.env.EMAIL)
+  try {
+    const info = await transporter.sendMail(mailOptions)
+    console.log('Email sent:', info.response)
+  } catch (error) {
+    console.log('error sending email', error)
+  }
+}
+const SendEmail = async (req, res) => {
+  try {
+    const { emails} = req.body;
+    const subject  = 'booking confirmation';
+    const text='your booking has been confirmed';
+    if (!emails || !subject || !text) {
+      return res.status(400).send('Missing required fields')
+    }
+    await sendAutoReplyEmail(emails, subject, text)
+    res.status(200).send('Email has been sent')
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
   GetBookings,
   CreateBooking,
+  SendEmail,
   BookingDetail,
   UpdateBooking,
   DeleteBooking,
